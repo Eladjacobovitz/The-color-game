734764fd6d88f457b335d19ad8960c09edd29190
Elad Jacobovitz <62474541+Eladjacobovitz@users.noreply.github.com>
2021-07-31 20:47:44 +0300

    Add files via upload

---
+++ client/public/colorgame.js
@@ -0,0 +1,53 @@
+const colorgame = `
+
+<head>
+ <title>Color Game</title>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="colorgame.css">
+</head>
+<body>
+
+
+ <h1>
+    <span id="name_of_user"></span>
+  <br>
+  <span id="colorDisplay">RGB</span>
+    <br>
+ Choose the right color
+ </h1>
+ <h2></h2>
+
+ <div id="stripe">
+  <button id="menu">Game menu</button>
+  <button id="reset">New Colors</button>
+  <span id="message"></span>
+  <button class="mode">Easy</button>
+  <button class="mode">Hard</button>
+  <button class="mode selected">Very hard</button>
+  <span id="timer"></span>
+ </div>
+ <div id="container">
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+ </div>
+ 
+ 
+
+</body>
+
+`
+
+
